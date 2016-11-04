dfcb106c9dc7940f99cb7239153cdd6c4a21cefa
Ksquare Inc <ksquareinc@Ksquares-MacBook-Pro-10.local>
2016-11-04 06:37:25 -0500

    created employee click toolbar

--- client/app/sidenav/employee/employee.controller.js
+++ client/app/sidenav/employee/employee.controller.js
@@ -2,71 +2,66 @@
 	'use strict';
 
 	angular.module('maerkApp')
-		.controller('employeesController', function ($scope, $mdToast, EmployeeService) {
+		.controller('employeesController', function ($scope, $mdToast, EmployeeService, $mdDialog, $mdSelect) {
 
-//		var that = this;
+		
 //		EmployeeService.get({id: 1}, function(data) {
 //			$scope.post = data;
 //		})
 		
-		$scope.employeeList = EmployeeService.query();
-//			$scope.employeeList = [
-//				{
-//					id: 1,
-//					name: 'Kendrick Carroll',
-//					client: 'Verizon',
-//					skills: 'UI/UX Designer',
-//					sales: 15,
-//					recruiter: 'Chris Young',
-//					revenue: 1000
-//            },
-//				{
-//					id: 2,
-//					name: 'Anish',
-//					client: 'Sabre',
-//					skills: 'UI/UX Designer',
-//					sales: 15,
-//					recruiter: 'Chris Young',
-//					revenue: 1000
-//            },
-//				{
-//					id: 3,
-//					name: 'Alisha',
-//					client: 'I dont know',
-//					skills: 'cook',
-//					sales: 15,
-//					recruiter: 'Home',
-//					revenue: 1000
-//            },
-//				{
-//					id: 4,
-//					name: 'Luis',
-//					client: 'Ksquare',
-//					skills: 'UI/UX Designer',
-//					sales: 15,
-//					recruiter: 'rocky',
-//					revenue: 1000
-//					},
-//				{
-//					id: 4,
-//					name: 'Luis',
-//					client: 'Ksquare',
-//					skills: 'UI/UX Designer',
-//					sales: 15,
-//					recruiter: 'rocky',
-//					revenue: 1000
-//					},
-//				{
-//					id: 4,
-//					name: 'Luis',
-//					client: 'Ksquare',
-//					skills: 'UI/UX Designer',
-//					sales: 15,
-//					recruiter: 'rocky',
-//					revenue: 1000
-//					}
-//					]
+
+		
+		$scope.showEdit = false;
+		$scope.showDelete = false;
+		$scope.showStatus = false;
+		
+		$scope.selectedRowCallback = function(rows) {
 			
-//			$scope.selectedEmployee = {id: Number, name: String, client: String};
+			if (rows == 0) {
+				$scope.showEdit = false;
+				$scope.showDelete = false;
+				$scope.showStatus = false;
+				console.log("0 rows selected");
+			}
+			
+			else if (rows.length == 1) {
+				$scope.showEdit = true;
+				$scope.showDelete = true;
+				$scope.showStatus = true;
+				console.log("1 row selected");
+			}
+			
+			else {
+				$scope.showEdit = false;
+				$scope.showDelete = true;
+				$scope.showStatus = true;
+				console.log('nothing selected')
+				console.log(rows);
+			}
+		}
+		
+		$scope.employeeList = EmployeeService.query();
+		
+		
+		//Delete confirmation code-------->		
+
+//  $scope.showConfirm = function(ev) {
+//    // Appending dialog to document.body to cover sidenav in docs app
+//    var confirm = $mdDialog.confirm()
+//          .title('Would you like to delete your debt?')
+//          .textContent('All of the banks have agreed to forgive you your debts.')
+//          .ariaLabel('Lucky day')
+//          .targetEvent(ev)
+//          .ok('Please do it!')
+//          .cancel('Sounds like a scam');
+//
+//    $mdDialog.show(confirm).then(function() {
+//      $scope.status = 'You decided to get rid of your debt.';
+//    }, function() {
+//      $scope.status = 'You decided to keep your debt.';
+//    });
+//  };
+		
+
 		});
 }());
