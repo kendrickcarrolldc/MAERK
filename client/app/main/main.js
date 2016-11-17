e02c56a3b933c7ddc10c4a5b2fff0d045c4bd4ea
Ksquare Inc <ksquareinc@Ksquares-MacBook-Pro-10.local>
2016-11-17 09:26:59 -0600

    setup skills view

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -2,7 +2,9 @@
 
 angular.module('maerkApp')
   .config(function($stateProvider) {
-    $stateProvider.state('main', {
+   
+	
+	$stateProvider.state('main', {
       url: '/dashboard',
       template: '<main flex layout="row"></main>',
 			authenticate: true,
@@ -15,6 +17,15 @@ angular.module('maerkApp')
 			controller: 'employeesController',
 			controllerAs: 'vm'
 //			authenticate: true
+		})
+		
+		.state('main.skills', {
+			url: '/skills',
+			templateUrl: 'app/sidenav/reports/skills/skills.html',
+			controller: 'skillsController',
+			controllerAs: 'vm',
+//			authenticate: true
+			
 		})
   });
 
