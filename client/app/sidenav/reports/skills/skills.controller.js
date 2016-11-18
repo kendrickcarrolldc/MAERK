3f8cf0f7ac593fea9fc05abed378635ab6b4b90b
Ksquare Inc <ksquareinc@Ksquares-MacBook-Pro-10.local>
2016-11-17 22:21:04 -0600

    skills table implemented

--- client/app/sidenav/reports/skills/skills.controller.js
+++ client/app/sidenav/reports/skills/skills.controller.js
@@ -10,65 +10,165 @@
 			
 		}
 		
-		this.skills = [
-            {
-                first_name: 'Luis',
-                last_name: 'Chapoy',
-                salary: 10000    
-            },
-             {
-                first_name: 'Nour',
-                last_name: 'Matouk',
-                salary: 9000   
-            },
+		this.report = function (newMonth) {
+			this.monthSelect = this.month[newMonth];
+			console.log(this.month['january'])
+			return this.month['january']
 			
-             {
-                first_name: 'Rocky',
-                last_name: 'karn',
-                salary: 3000   
+		}
+		
+	
+		
+		this.month = {
+			january: [{
+
+					first_name: 'Luis',
+					last_name: 'Chapoy',
+					salary: 10000
             },
-			
-             {
-                first_name: 'Jamie',
-                last_name: 'Liz',
-                salary: 9000
+				{
+					first_name: 'Nour',
+					last_name: 'Matouk',
+					salary: 9000
             },
-			
-             {
-                first_name: 'Anish',
-                last_name: 'Rijal',
-                salary: 8500
+
+				{
+					first_name: 'Rocky',
+					last_name: 'karn',
+					salary: 3000
             },
-			
-             {
-                first_name: 'Donovan',
-                last_name: 'Klang',
-                salary: 3000 
+
+				{
+					first_name: 'Jamie',
+					last_name: 'Liz',
+					salary: 9000
             },
-			
-             {
-                first_name: 'Long',
-                last_name: 'Pham',
-                salary: 2000
+
+				{
+					first_name: 'Anish',
+					last_name: 'Rijal',
+					salary: 8500
+
+
+			}],
+
+			february: [{
+
+					first_name: 'Long',
+					last_name: 'Pham',
+					salary: 2000
             },
-			
-            {
-                first_name: 'Raj',
-                last_name: 'K',
-                salary: 6000 
+
+				{
+					first_name: 'Raj',
+					last_name: 'K',
+					salary: 6000
             },
-			
-            {
-                first_name: 'Tarun',
-                last_name: 'M',
-                salary: 8000 
+
+				{
+					first_name: 'Tarun',
+					last_name: 'M',
+					salary: 8000
             },
-			
-            {
-                first_name: 'Lily',
-                last_name: 'Bather',
-                salary: 7000
-            },
-        ];
+
+				{
+					first_name: 'Lily',
+					last_name: 'Bather',
+					salary: 7000
+            }, ],
+
+			march: [{
+
+					first_name: 'Anish',
+					last_name: 'Rijal',
+					salary: 8500
+				},
+
+				{
+					first_name: 'Donovan',
+					last_name: 'Klang',
+					salary: 3000
+				},
+
+				{
+					first_name: 'Long',
+					last_name: 'Pham',
+					salary: 2000
+				},
+
+				{
+					first_name: 'Raj',
+					last_name: 'K',
+					salary: 6000
+
+			}]
+
+		};
+		
+		
+		this.monthSelect = [];
+		
+		this.report("march");
+		
+//		this.skills = [
+//            {
+//                first_name: 'Luis',
+//                last_name: 'Chapoy',
+//                salary: 10000    
+//            },
+//             {
+//                first_name: 'Nour',
+//                last_name: 'Matouk',
+//                salary: 9000   
+//            },
+//			
+//             {
+//                first_name: 'Rocky',
+//                last_name: 'karn',
+//                salary: 3000   
+//            },
+//			
+//             {
+//                first_name: 'Jamie',
+//                last_name: 'Liz',
+//                salary: 9000
+//            },
+//			
+//             {
+//                first_name: 'Anish',
+//                last_name: 'Rijal',
+//                salary: 8500
+//            },
+//			
+//             {
+//                first_name: 'Donovan',
+//                last_name: 'Klang',
+//                salary: 3000 
+//            },
+//			
+//             {
+//                first_name: 'Long',
+//                last_name: 'Pham',
+//                salary: 2000
+//            },
+//			
+//            {
+//                first_name: 'Raj',
+//                last_name: 'K',
+//                salary: 6000 
+//            },
+//			
+//            {
+//                first_name: 'Tarun',
+//                last_name: 'M',
+//                salary: 8000 
+//            },
+//			
+//            {
+//                first_name: 'Lily',
+//                last_name: 'Bather',
+//                salary: 7000
+//            },
+//        ];
 		});
 }());
