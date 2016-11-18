d00942acbe19436e5f4eb4e27853a3e7d40cb77b
Ksquare Inc <ksquareinc@Ksquares-MacBook-Pro-10.local>
2016-11-18 09:44:27 -0600

    working in the ajs google charts

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('maerkApp', ['maerkApp.auth', 'maerkApp.admin', 'maerkApp.constants', 'ngCookies',
-    'ngResource', 'ngSanitize', 'ui.router', 'validation.match','ngMaterial', 'mdDataTable', 'ngMessages'
+    'ngResource', 'ngSanitize', 'ui.router', 'validation.match','ngMaterial', 'mdDataTable', 'ngMessages', 'googlechart'
   ])
   .config(function($urlRouterProvider, $locationProvider, $mdThemingProvider) {
 		$mdThemingProvider.theme('default')
--- client/app/sidenav/reports/skills/skills.controller.js
+++ client/app/sidenav/reports/skills/skills.controller.js
@@ -110,65 +110,62 @@
 		
 		this.report("march");
 		
-//		this.skills = [
-//            {
-//                first_name: 'Luis',
-//                last_name: 'Chapoy',
-//                salary: 10000    
-//            },
-//             {
-//                first_name: 'Nour',
-//                last_name: 'Matouk',
-//                salary: 9000   
-//            },
-//			
-//             {
-//                first_name: 'Rocky',
-//                last_name: 'karn',
-//                salary: 3000   
-//            },
-//			
-//             {
-//                first_name: 'Jamie',
-//                last_name: 'Liz',
-//                salary: 9000
-//            },
-//			
-//             {
-//                first_name: 'Anish',
-//                last_name: 'Rijal',
-//                salary: 8500
-//            },
-//			
-//             {
-//                first_name: 'Donovan',
-//                last_name: 'Klang',
-//                salary: 3000 
-//            },
-//			
-//             {
-//                first_name: 'Long',
-//                last_name: 'Pham',
-//                salary: 2000
-//            },
-//			
-//            {
-//                first_name: 'Raj',
-//                last_name: 'K',
-//                salary: 6000 
-//            },
-//			
-//            {
-//                first_name: 'Tarun',
-//                last_name: 'M',
-//                salary: 8000 
-//            },
-//			
-//            {
-//                first_name: 'Lily',
-//                last_name: 'Bather',
-//                salary: 7000
-//            },
-//        ];
+		this.myChartObject = {};
+		
+		this.myChartObject.type = "PieChart";
+		
+//		this.chartingData = function (month, prop) {
+//			this.monthSelected = this.month[newMonth];
+//			console.log(this.month['january'])
+//			return this.month['january']
+//		}
+		
+		this.myChartObject.data = this.month.january;
+		
+		this.myChartObject.options = {
+        title: 'How Much Pizza I Ate Last Night',
+				width: 500,
+				height: 400
+    };
+		
+		
+//		this.onions = [{
+//				v: "Onions"
+//			},
+//        {
+//					v: 3
+//				},
+//		];
+		
+//		this.myChartObject.data = {
+//			"cols": [
+//        {id: "t", label: "Topping", type: "string"},
+//        {id: "s", label: "Slices", type: "number"}
+//    ], 
+//			"rows": [ // rows -array, object, object with an array with objects inside
+//        {c: [
+//            {v: "Mushrooms"},
+//            {v: 3},
+//        ]},
+//        {c: this.onions},
+//        {c: [
+//            {v: "Olives"},
+//            {v: 31}
+//        ]},
+//        {c: [
+//            {v: "Zucchini"},
+//            {v: 1},
+//        ]},
+//        {c: [
+//            {v: "Pepperoni"},
+//            {v: 2},
+//        ]}
+//    ]};
+		
+//[{{[]}}]
+								
+// rows in a array of objects
+								//each object has 
+
 		});
 }());
