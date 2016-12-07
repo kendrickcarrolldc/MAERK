8a46dcb8503e5f7b03b61995643f80c8b7ce9d87
Your Name <you@example.com>
2016-12-07 11:24:01 -0600

    report service created

---
+++ client/app/sidenav/reports/reports.service.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function () {
+	
+	angular.module('maerkApp')
+		.factory('ReportFact', function ($resource) {
+	
+	var reportResource = $resource('/api/report/:id/:controller', {
+		
+	}, {	
+	get: {
+					method: 'GET',
+					params: {
+						id: '@_id'
+					}
+				},
+				create: {
+					method: 'POST',
+				},
+				update: {
+					method: 'POST',
+				}	
+	});
+		
+		var reportList = reportResource.query();
+		
+		return {
+			reportList: reportList,
+			reportResource: reportResource,
+			create: function (report) {
+				new reportResource(report).$save().then(function (newReport) {
+					reportList.push(newReport)
+					
+				})
+			},
+			
+			update: function (month) {
+//			month.$save().then(function(updatedMonth) {
+//			console.log(updatedMonth);
+			
+		},
+		get: function () {
+			
+		},
+		getOne: function () {
+			
+		}	
+		}
+			
+		}
+});
+	
+})();
