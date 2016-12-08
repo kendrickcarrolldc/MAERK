27861e946369200d6a45289a5cfc011be51d22a4
Your Name <you@example.com>
2016-12-08 10:48:12 -0600

    set up controller, report

--- client/app/sidenav/register/register.controller.js
+++ client/app/sidenav/register/register.controller.js
@@ -11,6 +11,28 @@
 		this.month = ["January", "February", "March"];
 		
 		this.year = [2014, 2015, 2016];
+		
+//		this.report = {
+//			year: {
+//				type: Number,
+//				required: true
+//			},
+//			
+//			january: [employeeSchema],
+//			february: [employeeSchema],
+//			march: [employeeSchema],
+//			april: [employeeSchema],
+//			may: [employeeSchema],
+//			june: [employeeSchema],
+//			july: [employeeSchema],
+//			august: [employeeSchema],
+//			september: [employeeSchema],
+//			october: [employeeSchema],
+//			november: [employeeSchema],
+//			december: [employeeSchema]
+//
+//		}
+		
 
 	});
 })();
